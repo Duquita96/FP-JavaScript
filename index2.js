e3a86a4e84d00b1e4f269b2a127c37b0d2fbf237
Claudia <duquita96@gmail.com>
2023-11-06 16:25:14 +0100

    added sum on numbers

--- index2.js
+++ index2.js
@@ -2,70 +2,6 @@ const characters = {
   type: ["Elf", "Human", "Orc"],
 };
 
-function Race(input) {
-  let foundMatch = false;
-  for (let i = 0; i < characters.type.length; i++) {
-    if (input.toLowerCase() === characters.type[i].toLowerCase()) {
-      console.log(`You've chosen ${characters.type[i]}!`);
-      foundMatch = true;
-      break;
-    }
-  }
-  if (!foundMatch) {
-    userFeedback(
-      `You must write in character type, please try again.\n${characters.type[0]} - ${characters.type[1]} - ${characters.type[2]} \n`,
-      Race
-    );
-  }
-
-  // let chindex = characters.type.indexOf(input.toLowerCase())
-  //  if(chindex === -1){
-  //     userFeedback(
-  //     `You must write in character type, please try again.\n${characters.type[0]} - ${characters.type[1]} - ${characters.type[2]} \n`,
-  //     Race
-  //   );
-  //  } else {
-  //     console.log(`You've chosen ${characters.type[chindex]}!`);
-  //  }
-}
-
-function round(turno) {
-  console.log(`estas en el turno ${turno}`);
-  userFeedback("pregunta X del turno 1");
-}
-
-function startGame(callback) {
-  console.log("Welcome Player to the Game of Title/Game");
-  //Race();
-  // if (
-    userFeedback(
-      `First, type your Character:\n${characters.type[0]} - ${characters.type[1]} - ${characters.type[2]} \n`,
-      Race, callback
-    )
-  // )
-  //  { round("1");}
-  //return true;
-}
-
-function endGame() {
-  console.log("You have finished");
-}
-
-function Game() {
-  // if (startGame()) {
-  //   //le agrego un if porque si no se ejecuta simultaneamente las sig funciones antes que se termine la anterior
-  //   round("1");
-  //   //endGame();
-  // }
-  startGame(()=>{round("1");}
-
-  );
-  // round("1");
-  //endGame();
-}
-
-//***************************************** */
-
 function userFeedback(toAsk, myFunction, followUp) {
   const readline = require("readline");
   const rl = readline.createInterface({
@@ -74,16 +10,119 @@ function userFeedback(toAsk, myFunction, followUp) {
   });
   rl.question(toAsk, (userInput) => {
     myFunction(userInput);
-    followUp();
-    /*
-    para Confirmar:
-    myFunction() y followUp() son ambos funciones callback
-    myFunction() representa a la funcion que se desea ejecutar con la respuesta del usuario a la pregunta toAsk
-    followUp() representa a la funcion que se desea ejecutar DESPUES de que el usuario ha respondido */
+    followUp(userInput);
   });
 }
 
-//********************************************** */
+function round(callback) {
+  console.log("Calcula: 1 ejercicio:");
+  let level1 = 2;
+
+  function firstask(callback) {
+    userFeedback(
+      `\nWrite your answer.\n${level1} + ${level1}\n`,
+      function (input) {
+        if (input === "4") {
+          console.log("Correcto");
+          level1 = 4;
+          secondTask(callback);
+        }
+      },
+      function () {}
+    );
+  }
+  firstask(function () {});
+
+  let level2 = 40;
+  function secondTask(callback) {
+    userFeedback(
+      `\nWrite your answer.\n${level2} + ${level2}\n`,
+      function (input) {
+        if (input === "80") {
+          console.log("Lo estas haciendo genial!");
+          level2 = 80;
+          thirdTask(callback);
+        }
+        callback();
+      },
+      function () {}
+    );
+  }
+
+  let level3 = 120;
+  function thirdTask(callback) {
+    userFeedback(
+      `\nWrite your answer.\n${level3} + ${level3}\n`,
+      function (input) {
+        if (input === "240") {
+          console.log("Increible!!!");
+          level3 = 240;
+          lockCode(callback);
+        }
+        callback();
+      },
+      function () {}
+    );
+  }
+  callback;
+
+  function lockCode(callback) {
+    userFeedback(
+      `\nVamos a poner todos los resultados juntos en la cerradura! 
+  \nLock Code:\n`,
+      function (input) {
+        if (input === "480240") {
+          console.log("Está abierta!!!");
+        }
+        callback();
+      },
+      function () {}
+    );
+  }
+}
+
+function Race(input) {
+  let foundMatch = false;
+  for (let i = 0; i < characters.type.length; i++) {
+    if (input.toLowerCase() === characters.type[i].toLowerCase()) {
+      console.log(`You've chosen ${characters.type[i]}!\n`);
+      console.log(
+        `La historia es la siguiente. Eres un ${characters.type[i]} atrapado en una celda y para escapar. Para eso necesitas completar varias ejercicios\n`
+      );
+      round();
+      break;
+    } else {
+      foundMatch = true;
+      function wrongRace(callback) {
+        userFeedback(
+          `You must write in character type, please try again.\n${characters.type[0]} - ${characters.type[1]} - ${characters.type[2]} \n`,
+          Race,
+          callback
+        );
+      }
+      wrongRace();
+    }
+  }
+}
+
+function startGame(callback) {
+  console.log("Welcome Player to the Game of Title/Game");
+  userFeedback(
+    `First, type your Character:\n${characters.type[0]} - ${characters.type[1]} - ${characters.type[2]} \n`,
+    Race,
+    callback
+  );
+}
+
+function endGame() {
+  console.log("You have finished");
+}
+
+function Game() {
+  startGame(() => {
+    round(); //(" este es el valor de turno dentro de funcion Game(): turno#")
+  });
+}
 
 //*******EXE****//
 
